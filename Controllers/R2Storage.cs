8efa2bc76f86e470ce562f24476cb0150887d58f
alirehmani <osman.highbrow@gmail.com>
2023-09-18 18:11:09 +0500

    Add project files.

---
+++ Controllers/R2Storage.cs
@@ -0,0 +1,100 @@
+﻿using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace cloudfalre_r2_poc.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class R2Storage : ControllerBase
+    {
+
+        [HttpGet]
+        [Route(nameof(ListBuckets))]
+        
+        public async Task<IActionResult> ListBuckets()
+        {
+            try
+            {
+                var res=await cloudflare_r2.s3client().ListBucketsAsync();
+                
+                cloudflare_r2.s3client().Dispose();
+
+
+                return Ok(res.Buckets.ToList());
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+
+        [HttpGet]
+        [Route(nameof(GetBucketObject))]
+        public async Task<IActionResult> GetBucketObject(string bucketName)
+        {
+            //bucketName = "mytestbucket";
+            var req=new ListObjectsV2Request { BucketName = bucketName};
+            var res = await cloudflare_r2.s3client().ListObjectsV2Async(req);
+            Dictionary<string,string> result = new Dictionary<string,string>();
+
+            foreach (var obj in res.S3Objects)
+            {
+
+                result.Add("url", "https://anotherbucket.devops-ali.xyz/" + obj.Key);
+
+            }
+            
+            cloudflare_r2.s3client().Dispose();
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route(nameof(UploadObject))]
+        public async Task<IActionResult> UploadObject(IFormFile file)
+        {
+
+            try
+            {
+                var filepath=Path.GetTempFileName();
+                using (var stream=new FileStream(filepath,FileMode.Create))
+                {
+
+                    await file.CopyToAsync(stream);
+                }
+
+                var request = new PutObjectRequest
+                {
+
+                    FilePath = filepath,
+                    BucketName = "anotherbucket",
+                    DisablePayloadSigning = true,
+                    ContentType = file.ContentType,
+                    Key=file.FileName
+                    
+                };
+                var res = await cloudflare_r2.s3client().PutObjectAsync(request);
+
+                
+                return Ok("https://anotherbucket.devops-ali.xyz/"+file.FileName);
+                
+
+            }
+            catch (Exception e)
+            {
+                    
+                throw;
+            }
+
+            
+
+        }
+
+
+    }
+}
---
+++ Controllers/cloudflare_r2.cs
@@ -0,0 +1,21 @@
+﻿using Amazon.Runtime;
+using Amazon.S3;
+
+namespace cloudfalre_r2_poc.Controllers
+{
+    public static class cloudflare_r2
+    {
+        public static AmazonS3Client s3client()  {
+            var accessKey = "908d06c839990ab34a1e0a0ff802327f";
+            var secretKey = "e0f9e6345f4c7193410c17aae3b9e72186fd01cd53e4c0ce200d132fb23524ae";
+            var credentials = new BasicAWSCredentials(accessKey, secretKey);
+            var s3Client = new AmazonS3Client(credentials, new AmazonS3Config
+            {
+                ServiceURL = "https://692e82121988a4afdd33c6080043d72e.r2.cloudflarestorage.com",
+            });
+
+            return s3Client;
+
+        }
+    }
+}
